890bc6fbac370b74a714664804d815255af275c9
Daniel Alvarenga <danielalvarengalima@gmail.com>
2013-12-01 20:42:22 -0300

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LearnRails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root to:  'visitors#new'
@@ -14,6 +14,7 @@ LearnRails::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :contacts, only: [:new, :create]
 
   # Example resource route with options:
   #   resources :products do
